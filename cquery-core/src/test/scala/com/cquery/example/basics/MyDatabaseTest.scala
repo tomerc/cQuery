ffd3bc0c12a77683bbf6ad994b10fb74363a9d9b
deepaverma79 <deepaverma79@gmail.com>
2016-11-20 15:18:11 +0000

    Added Tests

---
+++ cquery-core/src/test/scala/com/cquery/example/basics/MyDatabaseTest.scala
@@ -0,0 +1,22 @@
+package com.cquery.example.basics
+
+import org.scalatest.{BeforeAndAfterAll, FlatSpec}
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+/**
+  * Created by Deepa on 20/11/2016.
+  */
+class MyDatabaseTest extends FlatSpec with BeforeAndAfterAll  with Defaults.connector.Connector {
+
+  override def beforeAll(): Unit = {
+  super.beforeAll()
+    Await.result(MyDatabase.autocreate().future(), 5.seconds)
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    Await.result(MyDatabase.autotruncate().future(), 5.seconds)
+  }
+}
+
